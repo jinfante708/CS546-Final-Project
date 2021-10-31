0f682967b0ea601143233fe8e134b40e7a8980a9
Jayson Infante <jaysoninfante@Jaysons-MacBook-Air.local>
2021-10-31 16:08:00 -0400

    Added index files for data/routes

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const tasklistsRoutes = require("./tasklists");
+const tasksRoutes = require("./tasks");
+const usersRoutes = require("./users");
+
+const constructorMethod = (app) => {
+  app.use("/taskslists", tasklistsRoutes);
+  app.use("/tasks", tasksRoutes);
+  app.use("./users", usersRoutes);
+
+  app.use("*", (req, res) => {
+    res.status(404).json({ error: "Not found" });
+  });
+};
+
+module.exports = constructorMethod;
