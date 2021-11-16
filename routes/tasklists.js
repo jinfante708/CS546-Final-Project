a6ec9855e275272ad9516c9b55f8e9cb68c28568
Keye Li <kli32@stevens.edu>
2021-11-16 15:20:52 -0500

    finish implementing data/tasklists.js

---
+++ routes/tasklists.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const data = require('../data');
+const taskListsData = data.tasklists;
+
+
+
+
+
+
+
+
+
+module.exports = router;
