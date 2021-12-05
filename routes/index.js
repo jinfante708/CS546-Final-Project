64f3dd873345e08d5161bb454bbb864e2f8886a4
AnkitKachhadiya <28569675+AnkitKachhadiya@users.noreply.github.com>
2021-12-05 00:15:40 -0500

    landing page design changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const tasklistsRoutes = require("./tasklists");
 const tasksRoutes = require("./tasks");
 const usersRoutes = require("./users");
+const path = require("path");
 
 const constructorMethod = (app) => {
     app.use("/taskslists", tasklistsRoutes);
@@ -12,14 +13,16 @@ const constructorMethod = (app) => {
 
     //for accessing unknown routes
     app.use("*", (request, response) => {
-        response.status(404).json({ serverResponse: "Not found." });
+        response
+            .status(404)
+            .sendFile(path.resolve("static/page-not-found.html"));
     });
 
     //for invalid URI
     app.use(function (error, request, response, next) {
         response
-            .status(error.statusCode)
-            .json({ serverResponse: "Bad Request." });
+            .status(404)
+            .sendFile(path.resolve("static/page-not-found.html"));
     });
 };
 
